41e6487f20c8c3f9886785d6120a6c1974b693e4
bankaizankanotatchi <naguejustin78@gmail.com>
2025-05-19 01:35:29 +0100

    ajout des entetes CORS dans login v2

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -169,7 +169,8 @@ export async function OPTIONS() {
 function setCorsHeaders(response: Response | NextResponse) {
   response.headers.set('Access-Control-Allow-Origin', process.env.NODE_ENV === 'development' 
     ? 'http://localhost:3000' 
-    : 'https://votre-frontend.com');
+    //: 'https://votre-frontend.com');
+    :'http://localhost:3000');
   response.headers.set('Access-Control-Allow-Methods', 'POST, OPTIONS');
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   response.headers.set('Access-Control-Allow-Credentials', 'true');
